087ffc0125ef43b53ef3cd508fa1beb445ce9a70
thebezze <jorge7bueno@gmail.com>
2022-03-27 10:00:36 +0200

    controlador de ticketLines

--- miproyecto/app/Http/Controllers/TicketLinesController.php
+++ miproyecto/app/Http/Controllers/TicketLinesController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\TicketLine;
 
 class TicketLinesController extends Controller
 {
-    //
+    public function store(Request $request){
+        $ticketline = new TicketLine;
+        $ticketline->cuotaElegida = $request->cuotaElegida;
+        $ticketline->game_id = $request->game_id;
+        $ticketline->ticket_id = $request->ticket_id;
+        $ticketline->save();
+
+        // falta devolver la vista/routa
+    }
+
+    public function shown($id){
+        $ticketline = TicketLine::find($id);
+        // falta devolver la vista
+
+    }
+
+    public function update(Request $request, $id){
+        $ticketline = TicketLine::find($id);
+        $ticketline->cuotaElegida = $request->cuotaElegida;
+        $ticketline->game_id = $request->game_id;
+        $ticketline->ticket_id = $request->ticket_id;
+        $ticketline->save();
+
+        // falta devolver la vista/routa
+
+    }
+
+    public function destroy($id){
+        $ticketline = TicketLine::find($id);
+        $ticketline->delete();
+
+        // falta devolver la vista/routa
+    }
 }
