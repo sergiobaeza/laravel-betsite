36a8b486a84d879917ae69b725887409dde6a76f
thebezze <jorge7bueno@gmail.com>
2022-05-19 11:29:59 +0200

    cambios solucionados

--- miproyecto/app/Models/User.php
+++ miproyecto/app/Models/User.php
@@ -4,11 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
  
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'balance',
+    ];
 
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    //protected $fillable = ['name','email','password','balance'];  
     public function tickets(){
         return $this->hasMany('App\Models\Ticket'); 
     }
